f90d98c291a1accb43d0591d8dacd5530dd29d33
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-09-30 18:51:44 -0300

    Add files via upload

---
+++ suicidio_capitalismo_comunismo.R
@@ -0,0 +1,25 @@
+
+# Suicídio ---------------------------------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 30/09/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/suicide -------------------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Morte por suicídio é uma questão extremamente complexa que causa dor a centenas
+### de milhares de pessoas a cada ano em todo o mundo. O objetivo desses dados é contribuir
+### para um informativo dados abertos sobre caminhos para prevenir o suicídio. Se você
+### está lidando com pensamentos suicidas você pode receber ajuda imediata por visitar
+### o Suicide.org ou fazer uma ligação para o 1-800-SUICIDE dos Estados Unidos.
+
+### Cada suicídio é uma tragédia. A Organização Mundial de Saúde e o  Global Burden of Disease
+### estimam que quase 800 mil pessoas morrem por suicídio todos os anos. Isto é uma
+### pessoa a cada 40 segundos.
+
+### Com o tempo, intervenções baseadas em evidências, os suicídios podem ser prevenidos.
+
+# Carregar pacotes -------------------------------------------------------------------------------------------------------------------------
+
+library(tidyverse)
+library(cols4all)
+library(hrbrthemes)
